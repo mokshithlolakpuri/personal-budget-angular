81c2b13c2766ca5ed2fd042f6de378a716463fbf
MokshithVarmaL <mokshithvarma02@gmail.com>
2024-09-15 22:51:11 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -3,27 +3,28 @@ const app = express();
 const port = 3000;
 
 app.use('/', express.static('public'));
+const budget = require('./budget-data.json');
 
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 30
-        },
-        {
-            title: 'Rent',
-            budget: 350
-        },
-        {
-            title: 'Groceries',
-            budget: 90
-        }
-    ]
-};
-
-app.get('/hello', (req, res) => {
-    res.send('Hello World!');
-});
+//const budget = {
+//    myBudget: [
+//        {
+//            title: 'Eat out',
+//            budget: 30
+//        },
+//        {
+//            title: 'Rent',
+//            budget: 350
+//        },
+//        {
+//            title: 'Groceries',
+//            budget: 90
+//        }
+//    ]
+//};
+app.use("/", express.static('public'))
+//app.get('/hello', (req, res) => {
+//    res.send('Hello World!');
+//});
 
 app.get('/budget', (req, res) => {
     res.json(budget);
