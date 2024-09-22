a55d1f60ee2a2fae96e9a081328a019577dbacf3
MokshithVarmaL <mokshithvarma02@gmail.com>
2024-09-21 22:31:34 -0400

    Added CORS

--- server.js
+++ server.js
@@ -1,35 +1,35 @@
+//Budget API
+
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.use('/', express.static('public'));
-const budget = require('./budget-data.json');
 
-//const budget = {
-//    myBudget: [
-//        {
-//            title: 'Eat out',
-//            budget: 30
-//        },
-//        {
-//            title: 'Rent',
-//            budget: 350
-//        },
-//        {
-//            title: 'Groceries',
-//            budget: 90
-//        }
-//    ]
-//};
-app.use("/", express.static('public'))
-//app.get('/hello', (req, res) => {
-//    res.send('Hello World!');
-//});
+app.use(cors());
+
+
+const budget = {
+    myBudget: [
+        {
+            title: 'Eat out',
+            budget: 30
+        },
+        {
+            title: 'Rent',
+            budget: 350
+        },
+        {
+            title: 'Groceries',
+            budget: 90
+        }
+    ]
+};
 
 app.get('/budget', (req, res) => {
     res.json(budget);
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`API served at http://localhost:3000`);
 });
